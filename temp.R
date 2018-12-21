12d7669179434080f72084357cd2376d53100a16
Babar Chaudary <babarathotmail@yahoo.com>
2018-12-21 10:06:18 +0200

    !

--- temp.R
+++ temp.R
@@ -1,14 +1,14 @@
-# result <- join_by_timestamp(data_ground_level[1:10, ], data2)
+result <- join_by_timestamp(data_ground_level[1:10, ], Isosuo)
 
 N <- nrow(data_ground_level)
-for (i in 1:N)   { 
-  tmp <- join_by_timestamp(data_ground_level[i, ], data2)
+for (i in 11:N)   { 
+  tmp <- join_by_timestamp(data_ground_level[i, ], Isosuo)
   result <- rbind(result, tmp)
   }
 
 save(result, file = "../Data/merged_Isosuo.R") 
 
-#load("../Data/merged_Isosuo.R")
+load("../Data/merged_Isosuo.R")
 
             
             
